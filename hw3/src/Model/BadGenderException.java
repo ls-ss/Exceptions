266fe0725ad4abc1ee8166c530ff68a72003a6ba
SergeyL <mkru@mail.ru>
2023-04-30 19:06:44 +0300

    Add hw3

---
+++ hw3/src/Model/BadGenderException.java
@@ -0,0 +1,15 @@
+package src.Model;
+
+public class BadGenderException extends Exception {
+    String inputString;
+
+    public BadGenderException(String inputString) {
+        this.inputString = inputString;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Неправильно указан пол (используйте только латинские буквы f или m), а не '" + inputString + "'\n";
+    }
+}
+
